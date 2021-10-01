a0d5a607a940f5c131974f1eb9259faa34f537d6
Atharva Salokhe <64686082+atharva123a@users.noreply.github.com>
2021-10-01 08:38:22 +0530

    commited images and supporting html and js files

---
+++ web/homepage/index.js
@@ -0,0 +1,6 @@
+// Here we will try to source a script where in when the user clicks the change to
+// dark mode button, we will basically change background text and border colors to suit the "dark mode".
+
+document.querySelector('#robert').onscroll = function() {
+    document.querySelector("#robert").style.fontWeight = 'bold';
+}
